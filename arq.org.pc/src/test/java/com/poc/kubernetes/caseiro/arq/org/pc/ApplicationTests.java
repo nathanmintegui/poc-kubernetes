287a180a3069aa9a14e49884dfee0fca35e79123
nathanmintegui <nathanberger.rs@hotmail.com>
2025-05-27 21:30:28 -0300

    init project 🚀

---
+++ arq.org.pc/src/test/java/com/poc/kubernetes/caseiro/arq/org/pc/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.kubernetes.caseiro.arq.org.pc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
