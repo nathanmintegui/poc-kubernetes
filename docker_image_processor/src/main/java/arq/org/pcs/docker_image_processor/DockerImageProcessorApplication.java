8621fe86a95bea2e26aab1685f779221e531b46e
fabiobastian <fabiobastian44@gmail.com>
2025-06-03 22:08:13 -0300

    initial commit

---
+++ docker_image_processor/src/main/java/arq/org/pcs/docker_image_processor/DockerImageProcessorApplication.java
@@ -0,0 +1,13 @@
+package arq.org.pcs.docker_image_processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerImageProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerImageProcessorApplication.class, args);
+	}
+
+}
